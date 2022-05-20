79a6d8275b78dbcbe93daa28459396140b70a586
Azat.Gumerov <Gumerov.AAg@gazpromneft-ntc.ru>
2022-05-20 19:37:20 +0300

    Description task 'all_upper_ii'

---
+++ all_upper_ii.py
@@ -0,0 +1,15 @@
+'''Check if a given string has all symbols in upper case. If the string is empty or doesn't have any letter in it - function should return False.
+
+Input: A string.
+
+Output: a boolean.
+Precondition: a-z, A-Z, 1-9 and spaces'''
+import re
+
+
+def is_all_upper(text: str) -> bool:
+    # your code here
+    return text.isupper()
+
+
+print(is_all_upper(""))
